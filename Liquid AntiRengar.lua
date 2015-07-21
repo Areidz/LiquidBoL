e92f4c64318eb26281cbe9d4af59cc56830613f7
Areidz <Areidz@users.noreply.github.com>
2015-07-21 14:43:41 +0200

    V 1.08

--- Liquid AntiRengar.lua
+++ Liquid AntiRengar.lua
@@ -3,9 +3,10 @@
 --Thanks to: Brown (Helping me testing and champion ideas)
 --To-Do: 	Add some other dashes, like Leblanc or Gnar.
 --			Add more champions and options.
---Version: 1.0.7
+--Version: 1.08
 
-require "VPrediction"
+local version = "1.08"
+_G.UseUpdater = true
 
 local champions = {	Ahri = {_E, 975, true, 1500, 0.25, 100}, 
 					Alistar = {_W, 650, false},
@@ -13,14 +14,73 @@ local champions = {	Ahri = {_E, 975, true, 1500, 0.25, 100},
 					--Azir R below life
 					Draven = {_E, 1050, true, 1400, 0.28, 90}, 
 					FiddleSticks = {_Q, 575, false},
-					Maokai = {_Q, 575, true, 1200, 0,5, 110},
+					--Janna Q : 1100, 900, 0.25, 120.
+					Janna = {_R, 725, false},
+					LeeSin = {_R, 375, false},
+					Maokai = {_Q, 575, true, 1200, 0.5, 110},
+					Syndra = {_E, 700, true, 2500, 0.25, 22.5},
 					Tristana = {_R, 550, false}, 
 					Vayne = {_E, 550, false}
 				}
 
 if not champions[myHero.charName] then return end
 
-local version = "1.0.7"
+local REQUIRED_LIBS = {
+	["VPrediction"] = "https://raw.githubusercontent.com/Hellsing/BoL/master/common/VPrediction.lua"
+}
+
+local DOWNLOADING_LIBS, DOWNLOAD_COUNT = false, 0
+
+function AfterDownload()
+	DOWNLOAD_COUNT = DOWNLOAD_COUNT - 1
+	if DOWNLOAD_COUNT == 0 then
+		DOWNLOADING_LIBS = false
+		print("<b><font color=\"#6699FF\">Liquid AntiRengar:</font></b> <font color=\"#FFFFFF\">Required libraries downloaded successfully, please reload (double F9).</font>")
+	end
+end
+
+for DOWNLOAD_LIB_NAME, DOWNLOAD_LIB_URL in pairs(REQUIRED_LIBS) do
+	if FileExist(LIB_PATH .. DOWNLOAD_LIB_NAME .. ".lua") then
+		if DOWNLOAD_LIB_NAME ~= "Prodiction" then require(DOWNLOAD_LIB_NAME) end
+		--if DOWNLOAD_LIB_NAME == "Prodiction" and VIP_USER then require(DOWNLOAD_LIB_NAME) end
+	else
+		DOWNLOADING_LIBS = true
+		DOWNLOAD_COUNT = DOWNLOAD_COUNT + 1
+		DownloadFile(DOWNLOAD_LIB_URL, LIB_PATH .. DOWNLOAD_LIB_NAME..".lua", AfterDownload)
+	end
+end
+
+if DOWNLOADING_LIBS then return end
+
+local UPDATE_NAME = "Liquid AntiRengar"
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/Areidz/LiquidBoL/master/Liquid%20AntiRengar.lua" .. "?rand=" .. math.random(1, 10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+local UPDATE_URL = "http://"..UPDATE_HOST..UPDATE_PATH
+
+function AutoupdaterMsg(msg) print("<b><font color=\"#6699FF\">"..UPDATE_NAME..":</font></b> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if _G.UseUpdater then
+	local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+	if ServerData then
+		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+		if ServerVersion then
+			ServerVersion = tonumber(ServerVersion)
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available "..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 2)	 
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
+
+require "VPrediction"
+
 local skillReady = false
 local skillRange, myChampion, antiRengar = nil, nil, nil
 
@@ -61,6 +121,8 @@ function OnNewPath(unit, startPos, endPos, isDash, dashSpeed, dashGravity, dashD
 					else
 						CastSpell(myChampion[1], unit.x, unit.z)
 					end
+				elseif myHero.charName == "Janna" then
+					CastSpell(myChampion[1])
 				else
 					CastSpell(myChampion[1], unit)
 				end
