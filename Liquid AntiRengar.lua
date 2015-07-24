99dbef3058abbd8b29ebf4153f12b5f550c54598
Areidz <Areidz@users.noreply.github.com>
2015-07-24 16:12:41 +0200

    V 1.13

--- Liquid AntiRengar.lua
+++ Liquid AntiRengar.lua
@@ -1,19 +1,24 @@
-local version = "1.12"
+local version = "1.13"
 _G.UseUpdater = true
 
 local champions = {	Ahri = {_E, 975, true, 1500, 0.25, 100}, 
 					Alistar = {_W, 650, false},
 					--Anivia Wall (Difficult)
 					Azir = {_R, 250, true, 2000, 0.25, 500}, --Not real values, just a test.
+					Braum = {_E, 250, true, 2000, 0.25, 500}, --Not real values, just a test.
 					Draven = {_E, 1050, true, 1400, 0.28, 90}, 
 					FiddleSticks = {_Q, 575, false},
 					--Janna Q : 1100, 900, 0.25, 120.
 					Janna = {_R, 725, false},
 					LeeSin = {_R, 375, false},
+					Lulu = {_W, 650, false},
+					Malzahar = {_R, 700, false},
 					Maokai = {_Q, 575, true, 1200, 0.5, 110},
+					Rammus = {_Q, 250, false},
 					Syndra = {_E, 700, true, 2500, 0.25, 22.5},
 					Tristana = {_R, 550, false}, 
-					Vayne = {_E, 550, false}
+					Vayne = {_E, 550, false},
+					XinZhao = {_R, 187.5, false}
 				}
 
 if not champions[myHero.charName] then return end
@@ -23,7 +28,7 @@ if not champions[myHero.charName] then return end
 --Thanks to: Brown (Helping me testing and champion ideas)
 --To-Do: 	Add some other dashes, like Leblanc or Gnar.
 --			Add more champions and options.
---Version: 1.12
+--Version: 1.13
 
 local REQUIRED_LIBS = {
 	["VPrediction"] = "https://raw.githubusercontent.com/Hellsing/BoL/master/common/VPrediction.lua"
@@ -121,7 +126,7 @@ function OnNewPath(unit, startPos, endPos, isDash, dashSpeed, dashGravity, dashD
 					else
 						DelayAction(function() CastSpell(myChampion[1], unit.x, unit.z) end, antiRengar.delayTime*0.001)
 					end
-				elseif myHero.charName == "Janna" then
+				elseif myHero.charName == "Janna" or myHero.charName == "XinZhao" or myHero.charName == "Rammus" then
 					DelayAction(function() CastSpell(myChampion[1]) end, antiRengar.delayTime*0.001)
 				else
 					DelayAction(function() CastSpell(myChampion[1], unit) end, antiRengar.delayTime*0.001)
